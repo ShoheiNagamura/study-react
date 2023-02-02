17998c1c3fbd5866144e73b0d0f35c37032067ae
shouhei <shohei.nagamura@gmail.com>
2023-02-02 15:17:18 +0900

    ♻️ ヘッダー、メイン画像部分、リンクをコンポーネント化

---
+++ components/mainImage.jsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export function MainImage() {
+    return (
+        <>
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+                <div className={styles.thirteen}>
+                    <Image
+                        src="/thirteen.svg"
+                        alt="13"
+                        width={40}
+                        height={31}
+                        priority
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
