624ab19b6cb19a5ef926835dbb17389c8e82caec
shouhei <shohei.nagamura@gmail.com>
2023-02-10 01:26:39 +0900

    💄 styleファイルを分割

--- components/headerSub.jsx
+++ components/headerSub.jsx
@@ -1,5 +1,5 @@
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from './headerSub.module.css'
 import { HeaderLogo } from '@/components/headerLogo'
 import { HeaderTitle } from '@/components/headerTitle'
 
