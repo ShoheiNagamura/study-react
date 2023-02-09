56538074899db3346807c6f94b8f1cd2e9285783
shouhei <shohei.nagamura@gmail.com>
2023-02-10 02:22:41 +0900

    ✨ ヘッダーリンクの追加

---
+++ components/headerLink.jsx
@@ -0,0 +1,23 @@
+import { Inter } from '@next/font/google';
+import Link from 'next/link'
+import styles from './headerLink.module.css';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export function HeaderLink(props) {
+    return (
+        <div className={styles.headerLink}>
+
+            <Link href="/" legacyBehavior>
+                <a className={styles.anchor}>Index</a>
+            </Link>
+
+            <Link href="/about" legacyBehavior>
+                <a className={styles.anchor}>About</a>
+            </Link>
+
+
+        </div >
+    )
+}
--- components/headerSub.jsx
+++ components/headerSub.jsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google'
 import styles from './headerSub.module.css'
 import { HeaderLogo } from '@/components/headerLogo'
 import { HeaderTitle } from '@/components/headerTitle'
+import { HeaderLink } from '@/components//headerLink'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +14,8 @@ export function HeaderSub(props) {
                 <HeaderTitle page={props.title}>
                     {<code className={styles.code}>pages/{props.page}.js</code>}
                 </HeaderTitle>
+
+                <HeaderLink />
 
                 <HeaderLogo />
             </div>
